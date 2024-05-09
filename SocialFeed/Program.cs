d57da70a71fbfb0ecfea0df512005cec46952b67
Siddhartha <siddhartha.pradhan.ix@gmail.com>
2024-05-09 15:40:12 +0545

    Old Fixes.

--- SocialFeed/Program.cs
+++ SocialFeed/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SocialFeed;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
+
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
